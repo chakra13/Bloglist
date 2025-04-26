a91ca6404a414dddb4161832bc51ffc23f03ecd7
chakra13 <chatrynguemen@gmail.com>
2025-04-26 01:16:53 +0100

    bloglist test

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/bloglist', bloglistRouter)
+app.use('/api/blogs', bloglistRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ tests/bloglist_api.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
